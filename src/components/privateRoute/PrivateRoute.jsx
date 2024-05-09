b201d7160f768cfbc926202659ed2d1f5960a002
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-05-09 23:49:35 +0300

    Update PrivateRoute.jsx

--- src/components/privateRoute/PrivateRoute.jsx
+++ src/components/privateRoute/PrivateRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { selectIsLoggedIn } from '../redux/auth/selectors';
+import { selectIsLoggedIn } from '../../redux/auth/selectors';
 
 export const PrivateRoute = ({ children, redirectTo = "/contacts" }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
