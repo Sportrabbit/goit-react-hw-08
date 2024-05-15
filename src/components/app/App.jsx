312e9af279dc374e34ff84fb9b69e1d0fd0b7bc3
Sportrabbit <145197781+Sportrabbit@users.noreply.github.com>
2024-05-15 20:29:41 +0300

    Update App.jsx

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, lazy, } from 'react';
+import { useEffect, lazy } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { Route, Routes } from 'react-router-dom';
 import { Layout } from '../layout/Layout';
